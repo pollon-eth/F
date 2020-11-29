d3df0aa95e38a454d2b0d902a487cc1512a833da
pollon-eth <75224481+pollon-eth@users.noreply.github.com>
2020-11-29 23:51:10 +0200

    Changes

--- README.md
+++ README.md
@@ -17,6 +17,11 @@
          alt="Create React App version" />
   </a>
   
+  <a href="https://github.com/pollon-eth/F/pulls">
+    <img src="https://badgen.net/github/prs/pollon-eth/F" alt="Pull Request Amount">
+  </a>
+  <img src="https://badgen.net/github/last-commit/pollon-eth/F" alt="Last commit date" />
+            
   <a href="./LICENSE">
     <img src="https://badgen.net/badge/license/MIT/blue"
          alt="License">
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,10 @@
 import React from "react";
 import theme from "theme";
-import { Theme, Link, Text, Box, Icon } from "@quarkly/widgets";
+import { Theme, Link, Text, Box, Icon, Input, Button } from "@quarkly/widgets";
 import { Helmet } from "react-helmet";
-import { StackItem, Stack, Section, Override } from "@quarkly/components";
-import { MdArrowDownward } from "react-icons/md";
+import { StackItem, Stack, Section, Override, Formspree } from "@quarkly/components";
+import { MdArrowDownward, MdLocationOn, MdEmail, MdPhone } from "react-icons/md";
+import { FaFacebookF, FaTwitter, FaLinkedinIn } from "react-icons/fa";
 export default (() => {
 	return <Theme theme={theme}>
 		<Helmet>
@@ -16,9 +17,13 @@ export default (() => {
 		<Section
 			padding="140px 0"
 			sm-padding="40px 0"
-			background="linear-gradient(0deg,rgba(0,0,0,.2) 0%,rgba(0,0,0,.2) 100%),--color-dark url(https://images.unsplash.com/photo-1486074051793-e41332bf18fc?ixlib=rb-1.2.1&auto=format&fit=crop&w=668&q=80) 50% 15%/cover"
+			background="linear-gradient(0deg,rgba(0,0,0,.2) 0%,rgba(0,0,0,.2) 100%),--color-dark url(https://uploads.quarkly.io/5fc41111dc290b001ef262fe/images/green.jpg?v=2020-11-29T21:42:40.982Z) 50% 15%/cover"
 			color="--light"
 			font="--base"
+			height="900px"
+			border-width="7px"
+			border-style="inherit"
+			border-color="#000000"
 		>
 			<Box>
 				<Text
@@ -31,9 +36,7 @@ export default (() => {
 					lg-font="200 42px/1.2 --fontFamily-sans"
 					lg-letter-spacing="20px"
 				>
-					Mary
-					<br />
-					Cornell
+					Tantaroba
 				</Text>
 			</Box>
 			<Stack>
@@ -46,24 +49,12 @@ export default (() => {
 					letter-spacing="1px"
 				>
 					<Text margin="0px">
-						About me
+						suca
 					</Text>
 				</StackItem>
-				<StackItem width="25%" lg-width="50%" sm-width="100%">
-					<Text margin="0px">
-						My name is Mary Cornell, born and raised in France.
-					</Text>
-				</StackItem>
-				<StackItem width="25%" lg-width="50%" sm-width="100%">
-					<Text margin="0px">
-						I've been working as a professional photographer and videographer for more than 12 years.
-					</Text>
-				</StackItem>
-				<StackItem width="25%" lg-width="50%" sm-width="100%">
-					<Text margin="0px">
-						CDS Documentary Essay Prize and Best Photographer of The Year 2019 by Sony World Photography Awards.
-					</Text>
-				</StackItem>
+				<StackItem width="25%" lg-width="50%" sm-width="100%" />
+				<StackItem width="25%" lg-width="50%" sm-width="100%" />
+				<StackItem width="25%" lg-width="50%" sm-width="100%" />
 			</Stack>
 		</Section>
 		<Section padding="140px 0" sm-padding="40px 0" background="url(https://images.unsplash.com/photo-1515600051222-a3c338ff16f6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80) center/cover">
@@ -79,10 +70,10 @@ export default (() => {
 					About me
 				</Text>
 				<Text as="h2" font="--headline2" margin="0 0 12px 0">
-					Hey I’m David
+					Hey I’m LAMADONNA
 				</Text>
 				<Text font="--base">
-					My name is David R. Moore, born and raised in France, worked as a professional photographer and videographer for more than 10 years, awarded the CDS Documentary Essay Prize in 2018 and Best Photographer of The Year 2019 by Sony World Photography Awards.
+					My name is LAMADONNA, born and raised in France, worked as a professional photographer and videographer for more than 10 years, awarded the CDS Documentary Essay Prize in 2018 and Best Photographer of The Year 2019 by Sony World Photography Awards.
 				</Text>
 			</Box>
 		</Section>
@@ -110,6 +101,249 @@ export default (() => {
 				</Text>
 				<Icon category="md" margin="0 auto" icon={MdArrowDownward} />
 			</Box>
+		</Section>
+		<Formspree errorMessage="Something went wrong" completeText="Success">
+			<Input name="email" placeholder="Email" margin-right="4px" />
+			<Button>
+				Submit
+			</Button>
+		</Formspree>
+		<Section padding="64px 0" sm-padding="40px 0" font="--base" color="--dark">
+			<Text
+				as="h1"
+				font="--headline1"
+				md-font="--headline2"
+				max-width="520px"
+				margin="0 auto 5px auto"
+				text-align="center"
+			>
+				We are IOBON
+			</Text>
+			<Text
+				as="p"
+				color="--grey"
+				max-width="520px"
+				margin="0 auto"
+				text-align="center"
+				font="--lead"
+			>
+				The smartest people work every day to provide the best service and to make our clients happy
+			</Text>
+			<Stack margin-top="40px">
+				<StackItem width="25%" lg-width="50%" sm-width="100%">
+					<Override slot="StackItemContent" flex-direction="column" />
+					<Box height="0" margin="0 0 20px 0" padding-bottom="100%" background="url(https://images.unsplash.com/photo-1606592399334-afc464f8d09f?ixlib=rb-1.2.1&q=85&fm=jpg&crop=entropy&cs=srgb&ixid=eyJhcHBfaWQiOjE0MjIzN30&h=2000) 50% 0/cover no-repeat" />
+					<Text color="--grey" margin="0">
+						CEO
+					</Text>
+					<Text as="h3" font="--headline3" margin="5px 0 20px 0">
+						Nathan K. Joe
+					</Text>
+					<Text as="p" margin="20px 0 5px 0">
+						This space is 100% editable. Use it to introduce a team member, describe their work experience and role within the company. This is also a great place to highlight a team member's strong sides.
+					</Text>
+				</StackItem>
+				<StackItem width="25%" lg-width="50%" sm-width="100%" sm-margin-top="24px">
+					<Override slot="StackItemContent" flex-direction="column" />
+					<Box height="0" margin="0 0 20px 0" padding-bottom="100%" background="url(https://images.unsplash.com/photo-1606654951863-70346cd12201?ixlib=rb-1.2.1&q=85&fm=jpg&crop=entropy&cs=srgb&ixid=eyJhcHBfaWQiOjE0MjIzN30&h=2000) 50% 0/cover no-repeat" />
+					<Text color="--grey" margin="0">
+						Developer
+					</Text>
+					<Text as="h3" font="--headline3" margin="5px 0 20px 0">
+						David R. Lema
+					</Text>
+					<Text as="p" margin="20px 0 5px 0">
+						This space is 100% editable. Use it to introduce a team member, describe their work experience and role within the company. This is also a great place to highlight a team member's strong sides.
+					</Text>
+				</StackItem>
+				<StackItem width="25%" lg-width="50%" sm-width="100%" sm-margin-top="24px">
+					<Override slot="StackItemContent" flex-direction="column" />
+					<Box height="0" margin="0 0 20px 0" padding-bottom="100%" background="url(https://images.unsplash.com/photo-1606561956792-cd1dc8b7f7dc?ixlib=rb-1.2.1&q=85&fm=jpg&crop=entropy&cs=srgb&ixid=eyJhcHBfaWQiOjE0MjIzN30&h=2000) 50% 0/cover no-repeat" />
+					<Text color="--grey" margin="0">
+						Marketing
+					</Text>
+					<Text as="h3" font="--headline3" margin="5px 0 20px 0">
+						Janet E. Morey
+					</Text>
+					<Text as="p" margin="20px 0 5px 0">
+						This space is 100% editable. Use it to introduce a team member, describe their work experience and role within the company. This is also a great place to highlight a team member's strong sides.
+					</Text>
+				</StackItem>
+				<StackItem width="25%" lg-width="50%" sm-width="100%" sm-margin-top="24px">
+					<Override slot="StackItemContent" flex-direction="column" />
+					<Box height="0" margin="0 0 20px 0" padding-bottom="100%" background="url(https://images.unsplash.com/photo-1606663974085-618468f72d3a?ixlib=rb-1.2.1&q=85&fm=jpg&crop=entropy&cs=srgb&ixid=eyJhcHBfaWQiOjE0MjIzN30&h=2000) 50% 0/cover no-repeat" />
+					<Text color="--grey" margin="0">
+						Designer
+						<br />
+					</Text>
+					<Text as="h3" font="--headline3" margin="5px 0 20px 0">
+						Claire R. Peery
+						<br />
+					</Text>
+					<Text as="p" margin="20px 0 5px 0">
+						This space is 100% editable. Use it to introduce a team member, describe their work experience and role within the company. This is also a great place to highlight a team member's strong sides.
+					</Text>
+				</StackItem>
+			</Stack>
+		</Section>
+		<Section
+			color="--light"
+			padding="100px 0"
+			sm-padding="40px 0"
+			position="relative"
+			background="linear-gradient(0deg,rgba(25, 30, 34, 0.8) 0%,rgba(25, 30, 34, 0.8) 100%),--color-darkL2 url(https://images.unsplash.com/photo-1547619292-240402b5ae5d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=975&q=80)"
+		>
+			<Stack>
+				<StackItem width="50%" md-width="100%">
+					<Override slot="StackItemContent" flex-direction="column" />
+					<Box
+						padding="0 0 0 64px"
+						sm-padding="64px 0 0 0"
+						margin="32px 0 0 0"
+						max-width="360px"
+						position="relative"
+					>
+						<Icon
+							position="absolute"
+							size="48px"
+							top="0"
+							left="0"
+							category="md"
+							icon={MdLocationOn}
+						/>
+						<Text as="h4" margin="6px 0" font="--base">
+							Visit us
+						</Text>
+						<Text as="p" margin="6px 0" font="--headline3">
+							4998 Hanover Street, New York, 10011
+						</Text>
+					</Box>
+					<Box
+						padding="0 0 0 64px"
+						sm-padding="64px 0 0 0"
+						margin="64px 0 0 0"
+						max-width="360px"
+						position="relative"
+					>
+						<Icon
+							position="absolute"
+							size="48px"
+							top="0"
+							left="0"
+							category="md"
+							icon={MdEmail}
+						/>
+						<Text as="h4" margin="6px 0" font="--base">
+							Email us
+						</Text>
+						<Text as="p" margin="6px 0" font="--headline3">
+							<Link href="mailto:hello@company.com" text-decoration="none" hover-text-decoration="underline" color="--light">
+								hello@company.com
+							</Link>
+						</Text>
+					</Box>
+					<Box padding="0 0 0 64px" margin="64px 0 0 0" max-width="360px" position="relative">
+						<Icon
+							position="absolute"
+							size="48px"
+							top="0"
+							left="0"
+							category="md"
+							icon={MdPhone}
+						/>
+						<Text as="h4" margin="6px 0" font="--base">
+							Call us
+						</Text>
+						<Text as="p" margin="6px 0" font="--headline3">
+							+1 (234) 567-89-00
+							<br />
+							+1 (234) 567-89-00
+						</Text>
+					</Box>
+					<Box
+						padding="0 0 0 64px"
+						sm-padding="0"
+						margin="48px 0"
+						max-width="360px"
+						position="relative"
+						display="flex"
+					>
+						<Icon
+							category="fa"
+							icon={FaFacebookF}
+							width="48px"
+							height="48px"
+							size="24px"
+							margin-right="16px"
+							background="--color-primary"
+							border-radius="50%"
+						/>
+						<Icon
+							category="fa"
+							icon={FaTwitter}
+							width="48px"
+							height="48px"
+							size="24px"
+							margin-right="16px"
+							background="--color-primary"
+							border-radius="50%"
+						/>
+						<Icon
+							category="fa"
+							icon={FaLinkedinIn}
+							width="48px"
+							height="48px"
+							size="24px"
+							margin-right="16px"
+							background="--color-primary"
+							border-radius="50%"
+						/>
+					</Box>
+				</StackItem>
+				<StackItem width="50%" md-width="100%">
+					<Box
+						max-width="360px"
+						padding="56px 48px"
+						margin="0 0 0 auto"
+						md-max-width="100%"
+						background="--color-dark"
+					>
+						<Text as="h3" font="--headline3" margin="0 0 20px 0">
+							Leave us message
+						</Text>
+						<Formspree>
+							<Stack gap="16px">
+								<StackItem width="100%">
+									<Override slot="StackItemContent" flex-direction="column" />
+									<Text font="--base" margin="0 0 4px 0">
+										Name
+									</Text>
+									<Input width="100%" name="name" />
+								</StackItem>
+								<StackItem width="100%">
+									<Override slot="StackItemContent" flex-direction="column" />
+									<Text font="--base" margin="0 0 4px 0">
+										Email
+									</Text>
+									<Input width="100%" type="email" name="email" />
+								</StackItem>
+								<StackItem width="100%">
+									<Override slot="StackItemContent" flex-direction="column" />
+									<Text font="--base" margin="0 0 4px 0">
+										Message
+									</Text>
+									<Input as="textarea" rows="4" width="100%" name="message" />
+								</StackItem>
+								<StackItem width="100%">
+									<Button background="--color-dark">
+										Send
+									</Button>
+								</StackItem>
+							</Stack>
+						</Formspree>
+					</Box>
+				</StackItem>
+			</Stack>
 		</Section>
 		<Link
 			font={"--capture"}
