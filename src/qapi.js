a0bab57b44fbeb4f902dcad4752c980f2e656ac6
pollon-eth <75224481+pollon-eth@users.noreply.github.com>
2020-11-29 23:30:14 +0200

    Initial commit

---
+++ src/qapi.js
@@ -0,0 +1,25 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"5fc41111dc290b001ef26301",
+				"5fc41111dc290b001ef26303"
+			]
+		},
+		"5fc41111dc290b001ef26301": {
+			"id": "5fc41111dc290b001ef26301",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"5fc41111dc290b001ef26303": {
+			"id": "5fc41111dc290b001ef26303",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "create-react-app"
+}
